9ff44c95813744dcd43677b7bee45e50ab3552f9
Islam Nashaat <islamnashaat03@gmail.com>
2026-01-16 00:29:55 +0200

    adding css file5

--- wp-content/themes/esdf-theme/functions.php
+++ wp-content/themes/esdf-theme/functions.php
@@ -18,7 +18,7 @@ require_once get_template_directory() . '/inc/theme-setup.php';
 function esdf_enqueue_scripts() {
     // Enqueue the main stylesheet from assets/css/
     wp_enqueue_style('esdf-bootstrap-grid', get_template_directory_uri() . '/assets/css/bootstrap-grid.min.css', array(), '5.3.3');
-    // wp_enqueue_style('esdf-main-style', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime(get_template_directory() . '/assets/css/style.css'));
+    wp_enqueue_style('esdf-main-style', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime(get_template_directory() . '/assets/css/style.css'));
 }
 add_action('wp_enqueue_scripts', 'esdf_enqueue_scripts');
 
