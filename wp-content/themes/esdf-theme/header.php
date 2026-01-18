e43278c2cd8b574292845e9afe7110035ed0dd67
Islam Nashaat <islamnashaat03@gmail.com>
2026-01-19 01:40:38 +0200

    adding css file 9

--- wp-content/themes/esdf-theme/header.php
+++ wp-content/themes/esdf-theme/header.php
@@ -5,102 +5,114 @@
  * @package ESDF_Theme
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; 
+if (!defined('ABSPATH')) {
+  exit;
 }
 ?>
 
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<?php wp_head(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
+  <?php wp_body_open(); ?>
 
-    <div class="overlay overlay-all"></div>
-    <!-- SCROLL TO TOP  -->
-    <span class="up"><i class="fa-solid fa-arrow-up"></i></span>
+  <div class="overlay overlay-all"></div>
+  <!-- SCROLL TO TOP  -->
+  <span class="up"><i class="fa-solid fa-arrow-up"></i></span>
 
-    <!-- START UPPER BAR  -->
-    <div class="fixed-nav fixed">
-      <div class="upper-bar">
-        <div class="container">
-          <div class="wrapper">
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand">
-              <!-- LOGO -->
-              <?php 
-                    $logo = get_field('site_logo', 'option');
-                    if ($logo): ?>
-                        <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>">
-                    <?php endif; ?>
-            </a>
-            
-            <div class="icons">
-             
-              <div class="search-div">
-                <form class="search-form" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-                  <div class="input-wrap">
-                    <input type="search" class="input-text"
-                      placeholder="<?php echo esc_attr( ( defined( 'ICL_LANGUAGE_CODE' ) && ICL_LANGUAGE_CODE === 'ar' ) ? 'عن ماذا تبحث؟' : 'What are you looking for?' ); ?>"
-                      name="s" value="<?php echo get_search_query(); ?>">
-                    <button type="submit" class="btn search-btn">
-                      <i class="fa-solid fa-magnifying-glass"></i>
-                    </button>
-                  </div>
-                </form>
-              </div>
-               <ul class="social-links">
-                <?php if ( have_rows( 'social_accounts', 'option' ) ) : ?>
-                  <?php while ( have_rows( 'social_accounts', 'option' ) ) : the_row(); ?>
-                    <li class="social-link">
-                      <a href="<?php echo esc_url( get_sub_field( 'social_link' ) ); ?>" target="_blank" rel="noopener">
-                        <i class="<?php echo esc_attr( get_sub_field( 'social_icon' ) ); ?>"></i>
-                      </a>
-                    </li>
-                  <?php endwhile; ?>
-                <?php endif; ?>
-            </ul>
-              <?php if ( defined( 'ICL_LANGUAGE_CODE' ) ) : ?>
-                <a class="languages" href="<?php echo esc_url( ICL_LANGUAGE_CODE === 'ar' ? '?lang=en' : '?lang=ar' ); ?>">
-                  <?php echo ICL_LANGUAGE_CODE === 'ar' ? 'En' : 'Ar'; ?>
-                </a>
+  <!-- START UPPER BAR  -->
+  <div class="fixed-nav fixed">
+    <div class="upper-bar">
+      <div class="container">
+        <div class="wrapper">
+          <a href="<?php echo esc_url(home_url('/')); ?>" class="navbar-brand">
+            <!-- LOGO -->
+            <?php
+            $logo = get_field('site_logo', 'option'); ?>
+            <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>">
+          </a>
+
+          <div class="icons">
+
+            <ul class="social-links">
+              <?php if (have_rows('social_accounts', 'option')): ?>
+                <?php while (have_rows('social_accounts', 'option')):
+                  the_row(); ?>
+                  <li class="social-link">
+                    <a href="<?php echo esc_url(get_sub_field('social_link')); ?>" target="_blank" rel="noopener">
+                      <i class="<?php echo esc_attr(get_sub_field('social_icon')); ?>"></i>
+                    </a>
+                  </li>
+                <?php endwhile; ?>
               <?php endif; ?>
-            </div>
+            </ul>
+
           </div>
         </div>
       </div>
-      <div class="nav-bar wow fadeInDown">
-        <div class="container">
-          <nav class="nav-flex">
-            
-            <?php wp_nav_menu( array('theme_location' => 'main_menu', 'container' => false, 'items_wrap' => '<ul class="navbar-links">%3$s</ul>',
-                    ) ); ?>
-            <a class="toggle-btn menu-btn" type="button">
-              <span class="bar bar--1"></span>
-              <span class="bar bar--2"></span>
-              <span class="bar bar--3"></span>
-            </a>
-          </nav>
-        </div>
-      </div>
     </div>
+    <div class="nav-bar wow fadeInDown">
+      <div class="container">
+        <nav class="nav-flex">
+          <?php wp_nav_menu(array(
+            'theme_location' => 'main_menu',
+            'container' => false,
+            'items_wrap' => '<ul class="navbar-links">%3$s</ul>',
+          )); ?>
+          <div class="icons">
 
-    <!-- END MOBILE  -->
-    <div class="navbar-mobile">
-      <div class="nav-flex">
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand">
-          <!-- LOGO -->
-          <img src="<?php echo get_field('site_logo', 'option'); ?>" alt="">
-        </a>
-        <a class="btn close-btn">
-          <i class="fa-solid fa-xmark"></i>
-        </a>
+            <?php if (defined('ICL_LANGUAGE_CODE')): ?>
+              <a class="languages" href="<?php echo esc_url(ICL_LANGUAGE_CODE === 'ar' ? '/en' : '/ar'); ?>">
+                <?php echo ICL_LANGUAGE_CODE === 'ar' ? 'En' : 'Ar'; ?>
+              </a>
+            <?php endif; ?>
+          </div>
+          <div class="search-div">
+            <form class="search-form" method="get" action="<?php echo esc_url(home_url('/')); ?>">
+              <div class="input-wrap">
+                <input type="search" class="input-text"
+                  placeholder="<?php echo esc_attr((defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE === 'ar') ? 'عن ماذا تبحث؟' : 'What are you looking for?'); ?>"
+                  name="s" value="<?php echo get_search_query(); ?>">
+                <button type="submit" class="btn search-btn">
+                  <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+              </div>
+            </form>
+          </div>
+          <button class="search-btn main-btn">
+            <?php lang_in('search', 'أبحث') ?> <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+          <a class="toggle-btn menu-btn" type="button">
+            <span class="bar bar--1"></span>
+            <span class="bar bar--2"></span>
+            <span class="bar bar--3"></span>
+          </a>
+        </nav>
       </div>
-      <?php wp_nav_menu( array('theme_location' => 'main_menu', 'container' => false, 'items_wrap' => '<ul class="mobile-links">%3$s</ul>',
-        ) ); ?>
     </div>
+  </div>
+
+  <!-- END MOBILE  -->
+  <div class="navbar-mobile">
+    <div class="nav-flex">
+      <a href="<?php echo esc_url(home_url('/')); ?>" class="navbar-brand">
+        <!-- LOGO -->
+        <img src="<?php echo get_field('site_logo', 'option'); ?>" alt="">
+      </a>
+      <a class="btn close-btn">
+        <i class="fa-solid fa-xmark"></i>
+      </a>
+    </div>
+    <?php wp_nav_menu(array(
+      'theme_location' => 'main_menu',
+      'container' => false,
+      'items_wrap' => '<ul class="mobile-links">%3$s</ul>',
+    )); ?>
+  </div>
   <!-- END NAVBAR MOBILE  -->
