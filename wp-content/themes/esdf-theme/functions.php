2b75271cfbaed059e82b3471b69996364393031b
Islam Nashaat <islamnashaat03@gmail.com>
2026-01-19 01:46:14 +0200

    adding css file 10

--- wp-content/themes/esdf-theme/functions.php
+++ wp-content/themes/esdf-theme/functions.php
@@ -1,7 +1,9 @@
 <?php
-if (!defined('ABSPATH')) exit;
+if (!defined('ABSPATH'))
+    exit;
 
-function esdf_theme_setup() {
+function esdf_theme_setup()
+{
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     add_theme_support('html5', array('search-form', 'comment-form', 'gallery', 'caption'));
@@ -15,10 +17,14 @@ add_action('after_setup_theme', 'esdf_theme_setup');
  */
 require_once get_template_directory() . '/inc/theme-setup.php';
 
-function esdf_enqueue_scripts() {
+function esdf_enqueue_scripts()
+{
     // Enqueue the main stylesheet from assets/css/
     wp_enqueue_style('esdf-bootstrap-grid', get_template_directory_uri() . '/assets/css/bootstrap-grid.min.css', array(), '5.3.3');
     wp_enqueue_style('esdf-main-style', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime(get_template_directory() . '/assets/css/style.css'));
+
+    // Enqueue FontAwesome
+    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css', array(), '6.5.1');
 }
 add_action('wp_enqueue_scripts', 'esdf_enqueue_scripts');
 
