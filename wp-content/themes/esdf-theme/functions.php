a640c82a65578bab5c14a629a3a6059f3369c6db
Islam Nashaat <islamnashaat03@gmail.com>
2026-01-27 07:41:18 +0200

    adding js  file 2.2

--- wp-content/themes/esdf-theme/functions.php
+++ wp-content/themes/esdf-theme/functions.php
@@ -19,12 +19,15 @@ require_once get_template_directory() . '/inc/theme-setup.php';
 
 function esdf_enqueue_scripts()
 {
+    // Enqueue FontAwesome
+    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css', array(), '6.5.1');
     // Enqueue the main stylesheet from assets/css/
     wp_enqueue_style('esdf-bootstrap-grid', get_template_directory_uri() . '/assets/css/bootstrap-grid.min.css', array(), '5.3.3');
     wp_enqueue_style('esdf-main-style', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime(get_template_directory() . '/assets/css/style.css'));
 
-    // Enqueue FontAwesome
-    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css', array(), '6.5.1');
+    // Enqueue the main script from assets/js/
+    wp_enqueue_script('esdf-main-script', get_template_directory_uri() . '/assets/js/main.js', array(), filemtime(get_template_directory() . '/assets/js/main.js'), true);
+
 }
 add_action('wp_enqueue_scripts', 'esdf_enqueue_scripts');
 
