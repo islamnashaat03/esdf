d16bc6d99ba4236616228c9d6bb6822ccc821c0f
Islam Nashaat <islamnashaat03@gmail.com>
2026-01-30 19:05:54 +0200

    adding hero 1

--- wp-content/themes/esdf-theme/index.php
+++ wp-content/themes/esdf-theme/index.php
@@ -3,16 +3,23 @@
 <main id="main-content">
     <section class="hero-section">
         <?php if (get_field('hero_select', 'option') == 'video' && get_field('hero_vedio', 'option')) : ?>
-            <div class="background-video hero-background">
+            <div class="background-video hero-background image-wrap">
                 <video autoplay muted loop playsinline>
                     <source src="<?php echo esc_url(get_field('hero_vedio', 'option')); ?>" type="video/mp4">
                 </video>
             </div>
         <?php elseif (get_field('hero_select', 'option') == 'image' && get_field('hero_img', 'option')) : ?>
-            <div class="background-image hero-background">
+            <div class="background-image hero-background image-wrap">
                 <img src="<?php echo esc_url(get_field('hero_img', 'option')); ?>" alt="Hero Image">
             </div>
         <?php endif; ?>
+        <div class="container">
+            <div class="hero-content">
+                <h1><?php echo get_field('hero_title', 'option'); ?></h1>
+                <p><?php echo get_field('hero_text', 'option'); ?></p>
+                <a href="#" class="main-btn"><?php echo get_field('hero_button_text', 'option'); ?></a>
+            </div>
+        </div>
     </section>   
 </main>
 
