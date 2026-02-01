10d0cd8e001a89fba85c0a5428474f9728c24cc9
Islam Nashaat <islamnashaat03@gmail.com>
2026-02-01 23:53:45 +0200

    adding hero 1.4

--- wp-content/themes/esdf-theme/index.php
+++ wp-content/themes/esdf-theme/index.php
@@ -15,9 +15,9 @@
         <?php endif; ?>
         <div class="hero-content">
             <div class="container">
-                <h1><?php echo get_field('hero_title', 'option'); ?></h1>
-                <p><?php echo get_field('hero_text', 'option'); ?></p>
-                <a href="#" class="main-btn"><?php echo get_field('hero_button_text', 'option'); ?></a>
+                <h2><?php echo get_field('hero_title', 'option'); ?></h2>
+                <h1><?php echo get_field('hero_text', 'option'); ?></h1>
+                <a href="#" class="main-btn"><?php lang_in('Join Us', 'انضم الينا'); ?></a>
             </div>
         </div>
     </section>
