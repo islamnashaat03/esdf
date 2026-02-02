69d7371f22e429d05895bfe6f1a5194bcc89cc1f
Islam Nashaat <islamnashaat03@gmail.com>
2026-02-02 21:28:10 +0200

    add asos 1.3

--- wp-content/themes/esdf-theme/index.php
+++ wp-content/themes/esdf-theme/index.php
@@ -32,28 +32,28 @@
         <div class="container">
             <div class="wrapper">
                 <div class="content">
-                    <h2 data-aos="fade-up" data-aos-delay="100"><?php echo get_field('about_title', 'option'); ?></h2>
-                    <div class="text" data-aos="fade-up" data-aos-delay="200"><?php echo get_field('about_text', 'option'); ?></div>
+                    <h2 data-aos="fade-up" data-aos-delay="50"><?php echo get_field('about_title', 'option'); ?></h2>
+                    <div class="text" data-aos="fade-up" data-aos-delay="100"><?php echo get_field('about_text', 'option'); ?></div>
                     <ul class="numbers">
-                        <li data-aos="fade-up" data-aos-delay="100">
+                        <li data-aos="fade-up" data-aos-delay="50">
                             <div class="number">15+</div>
                             <div class="text"><?php lang_in('Years of Experience', 'سنوات من الخبرة'); ?></div>
-                        </li>
-                        <li data-aos="fade-up" data-aos-delay="200">
+                        </li>       
+                        <li data-aos="fade-up" data-aos-delay="100">
                             <div class="number">500+</div>
                             <div class="text" ><?php lang_in('Active Members', 'الاعضاء النشطين'); ?></div>
                         </li>
-                        <li data-aos="fade-up" data-aos-delay="300">
+                        <li data-aos="fade-up" data-aos-delay="150">
                             <div class="number">16+</div>
                             <div class="text" ><?php lang_in('Annual Conferences', 'المؤتمرات السنوية'); ?></div>
                         </li>
-                        <li data-aos="fade-up" data-aos-delay="400">
+                        <li data-aos="fade-up" data-aos-delay="200">
                             <div class="number">3M+</div>
                             <div class="text" ><?php lang_in('Website Visitors', 'زوار الموقع'); ?></div>
                         </li>
                     </ul>
                 </div>
-                <div data-aos="fade-up" data-aos-delay="500" class="image">
+                <div data-aos="fade-up" data-aos-delay="250" class="image">
                     <img src="<?php echo esc_url(get_field('about_img', 'option')); ?>" alt="About Image">
                 </div>
             </div>
