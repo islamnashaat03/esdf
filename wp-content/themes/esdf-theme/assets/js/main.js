07dc6a2a3e3011f51e88a0cae24f6ab946edc3fe
Islam Nashaat <islamnashaat03@gmail.com>
2026-02-17 06:15:28 +0200

    add about page 3.1

--- wp-content/themes/esdf-theme/assets/js/main.js
+++ wp-content/themes/esdf-theme/assets/js/main.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Initialize AOS
   AOS.init({
-    duration: 1000,
+    duration: 500,
     easing: 'ease-in-out',
     once: true,
     mirror: true,
